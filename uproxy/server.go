afc40dc551f58167e635c18748daf18146e46514
Chris Lane <chris@lane-jayasinha.com>
2021-03-23 23:23:32 -0700

    at least one message got thru the whole round trip from an echo server

--- uproxy/server.go
+++ uproxy/server.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"fmt"
 	pb "github.com/jayalane/go-relay/udpProxy"
 	"io"
+	"net"
 	"sync"
 	"time"
 )
@@ -12,18 +14,113 @@ import (
 type udpProxyServer struct {
 	pb.UnimplementedProxyServer
 	// anything?  port -> chan map
-	portListeners    map[int]chan pb.UdpMsg
+	portListeners    map[string]chan *pb.UdpMsg
 	portListenerLock sync.RWMutex
 	inChan           chan *pb.UdpMsg // UDP from network to gRPC
 }
 
 func newProxyServer() *udpProxyServer {
 	s := udpProxyServer{}
+	s.portListeners = make(map[string]chan *pb.UdpMsg)
+	s.inChan = make(chan *pb.UdpMsg, 10000) // config
+	s.portListenerLock = sync.RWMutex{}
 	return &s
 }
 
 func (s *udpProxyServer) handleOutgoingMsg(m *pb.UdpMsg) {
-	ml.La("Got a msg", m)
+	ml.La("Got a msg from gRPC", m)
+	s.portListenerLock.RLock()
+	inAddr := fmt.Sprintf("%s:%s", m.SrcIp, m.SrcPort)
+	outAddr := fmt.Sprintf("%s:%s", m.DstIp, m.DstPort)
+	if ch, ok := s.portListeners[inAddr]; ok {
+		ml.La("found inAddr", inAddr, ok)
+		s.portListenerLock.RUnlock()
+		ml.La("putting msg in channel", m)
+		ch <- m
+		return
+	}
+	s.portListenerLock.RUnlock()
+	// update the listeners
+	s.portListenerLock.Lock()
+	val := make(chan *pb.UdpMsg, 10000) // config
+	s.portListeners[inAddr] = val
+	s.portListenerLock.Unlock()
+	// new go routine to handle the channel sends
+	go func(ch chan *pb.UdpMsg, outAddr string, inAddr string) {
+		ml.La("Got here outbound msg sends", ch, outAddr)
+		udpAddr, err := net.ResolveUDPAddr("udp", outAddr)
+		if err != nil {
+			ml.La("error resolving", outAddr, err)
+		}
+		conn, err := net.DialUDP("udp", nil, udpAddr)
+		if err != nil {
+			ml.La("Error Dialing", outAddr, err)
+			return
+		}
+		ml.La("Dial succeeded", udpAddr, conn.LocalAddr())
+		wg := sync.WaitGroup{}
+		defer conn.Close()
+		go func(conn *net.UDPConn, outAddr string, inAddr string) {
+			wg.Add(1)
+			defer wg.Done()
+			// and channel to handle the listen segments
+			for {
+				buffer := make([]byte, 4096)
+				ml.Ls("about to read from conn", conn)
+				n, ra, err := conn.ReadFrom(buffer)
+				if err != nil {
+					ml.Ls("got error on udp read", err)
+					return
+				}
+				ml.Ln("Read UDP msg", n, buffer[:n])
+				srcIP, srcPort, err := net.SplitHostPort(ra.String())
+				if err != nil {
+					ml.Ls("got error on split host port", ra)
+					continue
+				}
+				dstIP, dstPort, err := net.SplitHostPort(inAddr)
+				if err != nil {
+					ml.Ls("got error on split host port", inAddr)
+					return
+				}
+				m := &pb.UdpMsg{
+					SrcIp:   srcIP,
+					SrcPort: srcPort,
+					DstIp:   dstIP,
+					DstPort: dstPort,
+					Msg:     buffer[:n],
+				}
+				ml.Ln("sending inbound", m)
+				s.inChan <- m
+			}
+		}(conn, outAddr, inAddr)
+
+		defer ml.La("End of listen for Udp incoming", outAddr, inAddr)
+
+		for {
+			select {
+			case mm := <-ch:
+				n, err := conn.Write(mm.Msg)
+				if err != nil {
+					ml.Ls("Write error", err)
+					wg.Wait()
+					return
+				}
+				if n != len(mm.Msg) {
+					ml.Ls("Short write", n)
+					wg.Wait()
+					return
+				}
+				ml.Ln("Message sent", mm)
+			case <-time.After(60 * time.Second * 5): // config
+				ml.Ln("Timeout after 5 minutes on outbound")
+				wg.Wait()
+				return
+			}
+		}
+	}(val, outAddr, inAddr)
+	val <- m
+	ml.La("End of handle outgoing msg")
 }
 
 // SendMsgs receives a stream msgs which it puts on the wire and
@@ -37,9 +134,10 @@ func (s *udpProxyServer) SendMsgs(stream pb.Proxy_SendMsgsServer) error {
 				ml.La("Woot! Sending a msg in", note)
 				if err := ss.Send(note); err != nil {
 					done <- true
+					ml.La("client send got error", err)
 					return
 				}
-			case <-time.After(60 * time.Second):
+			case <-time.After(60 * time.Second * 5):
 				ml.La("No inbound msgs for a minute")
 				done <- true
 				return
@@ -51,9 +149,11 @@ func (s *udpProxyServer) SendMsgs(stream pb.Proxy_SendMsgsServer) error {
 		ml.La("Woot! got a message for out", out, err)
 		if err == io.EOF {
 			// done with reading
+			ml.Ls("EOF on gRPC read out")
 			break
 		}
 		if err != nil {
+			ml.Ls("on gRPC read out", err)
 			return err
 		}
 		// handle msg
