bf6b4f8d1d0587d702f9f5d9780619c066264467
Chris Lane <chris@lane-jayasinha.com>
2021-04-25 23:42:40 -0700

    try more squid

--- misc/setup_transproxy.sh
+++ misc/setup_transproxy.sh
@@ -29,8 +29,21 @@ set -e
 mkdir -p /etc/transproxy/
 # wget # TODO github releases
 
+iptables -F
+iptables -t nat -F 
+iptables -t mangle -F
+iptables -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+
 for p in 22 80 443  ; do
-    iptables -t nat -A PREROUTING -p tcp --dport $p --to-port 5999
+    iptables -t nat -A PREROUTING -p tcp --dport $p -j REDIRECT --to-port 5999
+done
+
+for p in 53  ; do
+    iptables -t nat -A PREROUTING -p udp --dport $p -j REDIRECT --to-port 1053
 done
 
 chmod 555 /etc/transproxy/transproxy
@@ -43,13 +56,16 @@ udpPorts = 1053
 destHostOverride = 
 destPortOverride = 
 numConnectionHandlers = 20
+squidHost = localhost
 squidPort = 4128
+squidForSNI = true
 profListen = localhost:6060
 #destCidrUseSquid = 45.33.0.0/16
 destCidrUseSquid = 0.0.0.0/0
+#destCidrUseSquid = 127.1.1.1/32
 srcCidrBan = 129.0.0.0/8
 # debug levels are (from low to high: network, state, all, none)
-debugLevel = network
+debugLevel = state
 # comments
 IOF
 
